25898c58ee5fa302f083da6d3e01b5dd06688a55
Chad <cpool7747@floridapoly.org>
2021-03-26 09:09:30 -0400

    small tweek

--- hers the game/tutorial14/Tileset.java
+++ hers the game/tutorial14/Tileset.java
@@ -7,8 +7,8 @@ import java.io.File;
 public class Tileset {
     public static Tile Tiles[] = 
     {
-    new tile("grass",'1',"tutorial14/texture/gras.gif"),
-    new tile("coin" ,'2',"tutorial14/texture/koin.png")
+    new Tile("grass",'1',"tutorial14/texture/gras.gif"),
+    new Tile("levelend" ,'2',"tutorial14/texture/koin.png")
     
     };
     
