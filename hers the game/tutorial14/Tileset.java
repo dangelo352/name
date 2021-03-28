29ee7e174356e523aa6bfe294cdaf934fbc54d39
JoshuaTheJosh <81395059+JoshuaTheJosh@users.noreply.github.com>
2021-03-28 03:25:33 -0400

    koin fix

--- hers the game/tutorial14/Tileset.java
+++ hers the game/tutorial14/Tileset.java
@@ -18,7 +18,7 @@ public class Tileset {
     new Tile("CornerBlueGrassTL", 'S', "tutorial14/texture/forest/CornerBlueGrassTL.GIF"),
     new Tile("RigtGrass", 'W', "tutorial14/texture/forest/RightGrass.GIF"),
     new Tile("LeftGrass", '1', "tutorial14/texture/forest/LeftGrass.GIF"),
-    new Tile("ForestCeiling", '2', "tutorial14/texture/forest/ForestCeiling.GIF"),
+    new Tile("ForestCeiling", ';', "tutorial14/texture/forest/ForestCeiling.GIF"),
     new Tile("CeilingBottom", '3', "tutorial14/texture/forest/CeilingBottom.GIF"),
     new Tile("FloatingForestBlock", '4', "tutorial14/texture/forest/FloatingForestBlock.GIF"),
     new Tile("CastleDirt", 'A', "tutorial14/texture/castle/CastleDirt.PNG"),
